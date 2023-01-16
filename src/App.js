5e5de2bc3540dda109f2baa664ee2b738c2300d5
Roman Porterfield <47892459+parallelmedias@users.noreply.github.com>
2023-01-16 12:38:53 -0500

    Will this work

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "react-toastify/dist/ReactToastify.css";
 import TaskList from "./components/TaskList";
 import Header from "./components/Header";
 import tasks from "./components/Task";
-import TaskForm from "./components/TaskForm";
 import TaskTotal from "./components/TaskTotal";
 
 export const URL = process.env.REACT_APP_SERVER_URL;
